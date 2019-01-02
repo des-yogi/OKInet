cc66676b2afd42d02c2234923232974c714cf4e1
des-yogi <nuevo-design@mail.ru>
2019-01-02 13:37:53 +0200

    form-js - fixed

--- src/blocks/form/form.js
+++ src/blocks/form/form.js
@@ -1,35 +1,36 @@
 (function(){
   //var form = document.getElementById('connection-request-form');
-  var submitBtn = document.getElementById('submit-form');
+  var submitBtn = document.getElementById('submit-form-btn');
   var agreementCtrl = document.getElementById('agreement');
   var agreementLbl = document.querySelector('label[for=agreement]');
 
   function makeChecked() {
+    agreementCtrl.setAttribute('checked', 'true');
     submitBtn.disabled = false;
     submitBtn.classList.remove('form__submit--disabled');
-    agreementCtrl.setAttribute('checked', 'true');
     agreementLbl.setAttribute('aria-pressed', 'true');
   };
 
   function makeUnChecked() {
+    agreementCtrl.removeAttribute('checked');
     submitBtn.disabled = true;
     submitBtn.classList.add('form__submit--disabled');
-    agreementCtrl.removeAttribute('checked');
     agreementLbl.setAttribute('aria-pressed', 'false');
   };
 
   var inputChangeState = function (e) {
     if ( agreementCtrl.checked ) {
       makeChecked();
+      //console.log('Checked');
     } else {
       makeUnChecked();
+      //console.log('UnChecked');
     }
   };
 
   var handleBtnKeyPress = function (e) {
     if (e.key === " " || e.key === "Enter") {
       e.preventDefault();
-      //console.log('Pressed');
       inputChangeState(e);
     }
   }
@@ -38,7 +39,7 @@
     agreementCtrl.addEventListener('change', inputChangeState);
   }
 
-  /*if (agreementLbl) {
-    agreementLbl.addEventListener('keydown', handleBtnKeyPress);
-  }*/
+  if (agreementLbl) {
+    agreementLbl.addEventListener('keypress', handleBtnKeyPress);
+  }
 }());
